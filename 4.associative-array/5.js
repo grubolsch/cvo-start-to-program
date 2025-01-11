e08975f0ef382aee56dab58268c02243a5be9630
Koen Eelen <koen.eelenpubliq.be>
2025-01-11 10:42:44 +0100

    add 4.5

--- 4.associative-array/5.js
+++ 4.associative-array/5.js
@@ -1,5 +1,16 @@
 function addFruit(fruits, name, color, taste, calories, carbs, fiber) {
     console.log(fruits, name, color, taste, calories, carbs, fiber);
 
+    fruits.push({
+        name: name, //Als zowel key als value hetzelfde zijn, kan je ook gewoon "name," schrijven.
+        color: color,
+        taste: taste,
+        nutrition: {
+            calories: calories,
+            carbs: carbs,
+            fiber: fiber
+        }
+    });
+    
     return fruits;
 }
